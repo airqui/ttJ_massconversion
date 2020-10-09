5379f2b04bbc79a4b7236240bbd2281d63e3a699
airqui <irles@lal.in2p3.fr>
2020-10-09 14:59:39 +0200

    minor updates

--- read_macros/include/histograms.h
+++ read_macros/include/histograms.h
@@ -12,5 +12,8 @@ double bwidth_ATLAS8TeV[] = {0.25, 0.075, 0.1, 0.1, 0.150, 0.05, 0.05,0.225};
 double bwidth_CMS13TeV[] = {0.18, 0.04, 0.05, 0.05, 0.06, 0.07,0.08, 0.09, 0.09, 0.29};
 
 
+//Including CMS
 string hnames[] = {"n3_ATLAS7TeV_pt30","n3_ATLAS7TeV_pt50","n3_ATLAS7TeV_pt75","n3_ATLAS7TeV_pt100","n3_ATLAS8TeV_pt30","n3_ATLAS8TeV_pt50","n3_ATLAS8TeV_pt75","n3_ATLAS8TeV_pt100","n3_12_pt30","n3_12_pt50","n3_12_pt75","n3_12_pt100","n3_24_pt30","n3_24_pt50","n3_24_pt75","n3_24_pt100","n3_CMS13TeV_pt30","n3_CMS13TeV_pt50","n3_CMS13TeV_pt75","n3_CMS13TeV_pt100","n3_CMS13TeV_pt30_eta2p4","n3_CMS13TeV_pt50_eta2p4","n3_CMS13TeV_pt75_eta2p4","n3_CMS13TeV_pt100_eta2p4"};
 
+//Not including CMS
+//string hnames[] = {"n3_ATLAS7TeV_pt30","n3_ATLAS7TeV_pt50","n3_ATLAS7TeV_pt75","n3_ATLAS7TeV_pt100","n3_ATLAS8TeV_pt30","n3_ATLAS8TeV_pt50","n3_ATLAS8TeV_pt75","n3_ATLAS8TeV_pt100","n3_12_pt30","n3_12_pt50","n3_12_pt75","n3_12_pt100","n3_24_pt30","n3_24_pt50","n3_24_pt75","n3_24_pt100"};
--- read_macros/read/launch.sh
+++ read_macros/read/launch.sh
@@ -1,17 +1,17 @@
 for mass in {156..178}
 do
 
-    ./ExtractData output LHC13 CT10 $mass 1 30 NLO > ../../logfiles/LHC13_CT10_mt${mass}_mu1_pt30.log &
-    ./ExtractData output LHC13 CT10 $mass 2 30 NLO > ../../logfiles/LHC13_CT10_mt${mass}_mu2_pt30.log &
-    ./ExtractData output LHC13 CT10 $mass 0.5 30 NLO > ../../logfiles/LHC13_CT10_mt${mass}_mu0.5_pt30.log
+    ./ExtractData output_LHC13_1 LHC13 CT10 $mass 1 30 NLO > ../../logfiles_LHC13_1/LHC13_CT10_mt${mass}_pt30_mu1.log &
+    ./ExtractData output_LHC13_1 LHC13 CT10 $mass 2 30 NLO > ../../logfiles_LHC13_1/LHC13_CT10_mt${mass}_pt30_mu2.log &
+    ./ExtractData output_LHC13_1 LHC13 CT10 $mass 0.5 30 NLO > ../../logfiles_LHC13_1/LHC13_CT10_mt${mass}_pt30_mu0.5.log
 done
 
 for mass in 155.5 156.5 157.5 158.5 159.5 160.5 161.5 162.5 163.5 164.5 165.5 166.5 167.5 168.5 169.5 170.5 171.5 172.5 173.5 174.5 175.5 176.5 177.5
 do
     echo $mass
-    ./ExtractData output LHC13 CT10 $mass 1 30 LO > ../../logfiles/LHC13_CT10_mt${mass}_mu1_pt30.log &
-    ./ExtractData output LHC13 CT10 $mass 2 30 LO > ../../logfiles/LHC13_CT10_mt${mass}_mu2_pt30.log &
-    ./ExtractData output LHC13 CT10 $mass 0.5 30 LO > ../../logfiles/LHC13_CT10_mt${mass}_mu0.5_pt30.log
+    ./ExtractData output_LHC13_1 LHC13 CT10 $mass 1 30 LO > ../../logfiles_LHC13_1/LHC13_CT10_mt${mass}_pt30_mu1.log &
+    ./ExtractData output_LHC13_1 LHC13 CT10 $mass 2 30 LO > ../../logfiles_LHC13_1/LHC13_CT10_mt${mass}_pt30_mu2.log &
+    ./ExtractData output_LHC13_1 LHC13 CT10 $mass 0.5 30 LO > ../../logfiles_LHC13_1/LHC13_CT10_mt${mass}_pt30_mu0.5.log
 done
 
 
